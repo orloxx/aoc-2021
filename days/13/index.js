854b2cb066a98c75f68765f06f4409b7a7d76ec5
Iker Garitaonandia <orloxx@users.noreply.github.com>
2022-12-13 13:29:14 +0100

    Day 13

---
+++ days/13/index.js
@@ -0,0 +1,76 @@
+import assert from 'assert'
+import read from '../../utils/read.js'
+
+function parseInput(list) {
+  return list.reduce((acc, line) => {
+    if (!acc.length) return [[JSON.parse(line)]]
+    if (line) {
+      const last = [...acc.pop(), JSON.parse(line)]
+      return [...acc, last]
+    }
+    return [...acc, []]
+  }, [])
+}
+
+function check(o1, o2) {
+  if (typeof o1 === 'number' && typeof o2 === 'number') {
+    const rest = o2 - o1
+    if (rest === 0) return rest
+    return rest / Math.abs(rest)
+  }
+  const [one, two] = [
+    typeof o1 === 'number' ? [o1] : o1,
+    typeof o2 === 'number' ? [o2] : o2,
+  ]
+
+  for (let i = 0; i < one.length; i++) {
+    if (typeof two[i] === 'undefined') return -1
+
+    const valid = check(one[i], two[i])
+
+    if (valid !== 0) return valid
+  }
+
+  if (one.length < two.length) return 1
+  return 0
+}
+
+function solution01(list) {
+  const data = parseInput(list)
+
+  return data.reduce((acc, pair, i) => {
+    const [one, two] = pair
+    const correct = check(one, two)
+
+    if (correct === 1) return acc + i + 1
+
+    return acc
+  }, 0)
+}
+
+function solution02(list) {
+  const two = [[2]]
+  const six = [[6]]
+  const data = parseInput(list).flat().concat([two], [six])
+
+  return data
+    .sort((a, b) => check(b, a))
+    .map((item, i) => {
+      if (item === two || item === six) {
+        return i + 1
+      }
+      return 0
+    })
+    .filter((n) => n > 0)
+    .multiplyAll()
+}
+
+read('test.txt').then((list) => {
+  assert.deepEqual(solution01(list), 13)
+  assert.deepEqual(solution02(list), 140)
+})
+
+read('input.txt').then((list) => {
+  assert.deepEqual(solution01(list), 6484)
+  assert.deepEqual(solution02(list), 399)
+})
