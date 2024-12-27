0715b6192f0e48453a85ac4c8b90099f2059d15b
Iker Garitaonandia <orloxx@users.noreply.github.com>
2024-12-27 21:11:08 +0100

    Day 18 (slow)

--- days/18/index.js
+++ days/18/index.js
@@ -3,7 +3,7 @@ import read from '../../utils/read.js'
 import { OBJ, treeFromGrid } from '../../utils/grid.js'
 import bfs from '../../utils/bfs.js'
 
-function parseInput(list, size) {
+function getWalledGrid(list, size) {
   return list.reduce((acc, line) => {
     const [x, y] = line.split(',').map(Number)
 
@@ -14,20 +14,37 @@ function parseInput(list, size) {
 }
 
 function solution01(list, size, timeBytes) {
-  const grid = parseInput(list.slice(0, timeBytes), size)
+  const grid = getWalledGrid(list.slice(0, timeBytes), size)
   const tree = treeFromGrid(grid)
 
   return bfs({ tree, start: '0,0', end: `${size},${size}` }).length - 1
 }
 
-function solution02(list) {}
+function solution02(list, size, timeBytes) {
+  const grid = getWalledGrid(list.slice(0, timeBytes), size)
+  const bytes = []
+  let path = [-1]
+
+  while (path.length) {
+    const [cx, cy] = list[timeBytes + bytes.length].split(',').map(Number)
+
+    grid[cy][cx] = OBJ.wall
+    bytes.push([cy, cx])
+
+    const tree = treeFromGrid(grid)
+
+    path = bfs({ tree, start: '0,0', end: `${size},${size}` })
+  }
+
+  return bytes[bytes.length - 1]
+}
 
 read('test.txt').then((list) => {
   assert.deepEqual(solution01(list, 6, 12), 22)
-  // assert.deepEqual(solution02(list, 6), 0)
+  assert.deepEqual(solution02(list, 6, 12), [1, 6])
 })
 
 read('input.txt').then((list) => {
   assert.deepEqual(solution01(list, 70, 1024), 384)
-  // assert.deepEqual(solution02(list, 70), 0)
+  assert.deepEqual(solution02(list, 70, 1024), [10, 36])
 })
